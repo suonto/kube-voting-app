3de085904d77230729e1314e12d1648035831ff6
Markus Suonto <markus.suonto@eficode.com>
2016-04-15 21:30:23 +0300

    Initial commit

---
+++ create.sh
@@ -0,0 +1,4 @@
+kubectl create -f redis-service.yaml
+kubectl create -f redis-deployment.yaml
+kubectl create -f voting-app-service.yaml
+kubectl create -f voting-app-deployment.yaml
---
+++ delete.sh
@@ -0,0 +1,4 @@
+kubectl delete service voting-app
+kubectl delete deployment voting-app
+kubectl delete deployment redis
+kubectl delete service redis
